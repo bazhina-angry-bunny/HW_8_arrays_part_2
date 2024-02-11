749edd11cdace6a5b3481a0cb3dd5396210f377d
bazhina-angry-bunny <praga617@mail.ru>
2024-02-11 14:33:36 +0500

    Ledger. Arrays.part.2

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,50 @@
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
+        task1();
+    }
 
-        System.out.println("Hello world!");
+    public static int[] generateRandomArray() {
+        java.util.Random random = new java.util.Random();
+        int[] ledgerArrays = new int[30];
+        for (int i = 0; i < ledgerArrays.length; i++) {
+            ledgerArrays[i] = random.nextInt(100_000) + 100_000;
+        }
+        return ledgerArrays;
+    }
+
+    public static void task1() {
+        int[] ledgerArrays = generateRandomArray();
+        System.out.println(Arrays.toString(ledgerArrays));
+        System.out.println();
+
+        int sum = 0;
+        for (int element : ledgerArrays) {
+            sum += element;
+        }
+        System.out.println("Сумма трат за месяц составила " + sum + " рублей");
+
+        int maxPayment = -1;
+        for (int i = 0; i < ledgerArrays.length; i++) {
+            if (ledgerArrays[i] > maxPayment) {
+                maxPayment = ledgerArrays[i];
+            }
+        }
+        int minPayment = 210_000;
+        for (int i = 0; i < ledgerArrays.length; i++) {
+            if (ledgerArrays[i] < minPayment) {
+                minPayment = ledgerArrays[i];
+            }
+        }
+        System.out.println("Минимальная сумма трат за день составила " + minPayment + " рублей. Максимальная сумма трат за день составила " + maxPayment + " рублей");
+
+        double monthlyAverageSpend = (double) sum / ledgerArrays.length;
+        System.out.println("Средняя сумма трат за месяц составила " + monthlyAverageSpend + " рублей.");
+
+        char[] reverseFullName = {'n', 'a', 'v', 'I', ' ', 'v', 'o', 'n', 'a', 'v', 'I'};
+        for (int i = reverseFullName.length - 1; i >= 0; i--) {
+            System.out.print(reverseFullName[i]);
+        }
     }
 }
